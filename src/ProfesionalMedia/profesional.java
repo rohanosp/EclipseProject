b805ac9d83b55fa0831576cc24e3654f74f980ab
rohanosp <rohan@osplabs.com>
2019-09-09 15:56:41 +0530

    Bhavika project

---
+++ src/ProfesionalMedia/profesional.java
@@ -0,0 +1,24 @@
+package ProfesionalMedia;
+
+import org.testng.annotations.Test;
+
+public class profesional {
+	@Test
+	public void Linkedin() 
+	{
+	System.out.println("Linkedin");
+
+	}
+	@Test
+	public void Glassdoor() 
+	{
+	System.out.println("Glassdoor");
+
+	}
+	@Test
+	public void Insight() 
+	{
+	System.out.println("Insight");
+
+	}
+}
---
+++ src/SocialMedia/social.java
@@ -0,0 +1,24 @@
+package SocialMedia;
+
+import org.testng.annotations.Test;
+
+public class social {
+	@Test
+	public void Twitter() 
+	{
+	System.out.println("Twitter");
+
+	}
+	@Test
+	public void FaceBook() 
+	{
+	System.out.println("Facebook");
+
+	}
+	@Test
+	public void Instagram() 
+	{
+	System.out.println("Instagram");
+
+	}
+}
